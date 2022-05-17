6da92ee0ddc4a087cad02ed3e6b41ee3271d5a8d
iyegoroff <iegoroff@gmail.com>
2022-05-17 04:10:29 +0300

    fixes

--- fix-links.js
+++ fix-links.js
@@ -1,17 +1,20 @@
 const { promisify } = require('util')
-const fs = require('fs')
+const {
+  promises: { readlink, symlink, unlink, lstat },
+  existsSync
+} = require('fs')
 const process = require('process')
 const glob = promisify(require('glob'))
-
-const readlink = promisify(fs.readlink)
-const symlink = promisify(fs.symlink)
-const unlink = promisify(fs.unlink)
 
 glob('packages/*/node_modules/@ts-elmish/*')
   .then((files) => Promise.all(files.map((file) => readlink(file).then((link) => [link, file]))))
   .then((links) =>
     Promise.all(
-      links.map(([target, path]) => unlink(path).then(() => symlink(`../../${target}`, `${path}`)))
+      links.map(([target, path]) =>
+        existsSync(path)
+          ? undefined
+          : unlink(path).then(() => symlink(`../../${target}`, `${path}`))
+      )
     )
   )
   .catch((err) => {
